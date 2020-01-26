8f49c350182e4ced258cdb9af47725d127583727
VANMARCKE <timothey.vanmarcke@capgemini.com>
2020-01-26 19:13:43 +0100

    Ajout des constructeurs sans argument

--- src/main/java/com/miage/altea/tp/pokemon_type_api/bo/Stats.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/bo/Stats.java
@@ -5,4 +5,47 @@ public class Stats {
     private Integer defense;
     private Integer attack;
     private Integer hp;
+
+    public Stats() {
+        super();
+    }
+
+    public Stats(Integer speed, Integer defense, Integer attack, Integer hp) {
+        this.speed = speed;
+        this.defense = defense;
+        this.attack = attack;
+        this.hp = hp;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    public Integer getDefense() {
+        return defense;
+    }
+
+    public void setDefense(Integer defense) {
+        this.defense = defense;
+    }
+
+    public Integer getAttack() {
+        return attack;
+    }
+
+    public void setAttack(Integer attack) {
+        this.attack = attack;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
 }
