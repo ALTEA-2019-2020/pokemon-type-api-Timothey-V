5903f8f879b37a86219075d367bea77364363fd1
Timothey-V <Tim170298>
2020-02-10 13:08:11 +0100

    Add interoperability TP6

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/TranslationRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+import com.miage.altea.tp.pokemon_type_api.bo.Translation;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+@Repository
+public class TranslationRepositoryImpl implements TranslationRepository {
+
+    private Map<Locale, List<Translation>> translations;
+
+    private List<Translation> defaultTranslations;
+
+    public TranslationRepositoryImpl() {
+        try {
+            var objectMapper = new ObjectMapper();
+
+            var frenchTranslationStream = new ClassPathResource("translations-fr.json").getInputStream();
+            var frenchTranslationsArray = objectMapper.readValue(frenchTranslationStream, Translation[].class);
+
+            var englishTranslationStream = new ClassPathResource("translations-en.json").getInputStream();
+            var englishTranslationsArray = objectMapper.readValue(englishTranslationStream, Translation[].class);
+
+            this.translations = Map.of(
+                    Locale.FRANCE, List.of(frenchTranslationsArray),
+                    Locale.FRENCH, List.of(frenchTranslationsArray),
+                    Locale.ENGLISH, List.of(englishTranslationsArray)
+            );
+
+            this.defaultTranslations = List.of(englishTranslationsArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getPokemonName(int id, Locale locale) {
+        List<Translation> pokemons = translations.get(locale);
+        for (Translation pokemon : pokemons) {
+            if (pokemon.getId() == id)
+                return pokemon.getName();
+        }
+        return null;
+    }
+}
--- src/main/java/com/miage/altea/tp/pokemon_type_api/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/service/PokemonTypeServiceImpl.java
@@ -1,31 +1,68 @@
 package com.miage.altea.tp.pokemon_type_api.service;
 
+import com.fasterxml.jackson.databind.node.POJONode;
 import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
 import com.miage.altea.tp.pokemon_type_api.repository.PokemonTypeRepository;
+import com.miage.altea.tp.pokemon_type_api.repository.TranslationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class PokemonTypeServiceImpl implements PokemonTypeService{
-
+    @Autowired
     protected PokemonTypeRepository pokemonTypeRepository;
+    protected TranslationRepository translationRepository;
 
     public PokemonTypeServiceImpl(PokemonTypeRepository pokemonTypeRepository){
         this.pokemonTypeRepository = pokemonTypeRepository;
 
     }
 
+    public PokemonTypeServiceImpl() {
+    }
+
     @Override
     public PokemonType getPokemonType(int id) {
-        return pokemonTypeRepository.findPokemonTypeById(id);
+        PokemonType p = pokemonTypeRepository.findPokemonTypeById(id);
+        p.setName(translationRepository.getPokemonName(id, LocaleContextHolder.getLocale()));
+        return p;
+
     }
 
     @Override
     public List<PokemonType> getAllPokemonTypes(){
-        return pokemonTypeRepository.findAllPokemonType();
+        List<PokemonType> listPoke = pokemonTypeRepository.findAllPokemonType();
+        for (PokemonType pokemon:listPoke) {
+           pokemon.setName(translationRepository.getPokemonName(pokemon.getId(),LocaleContextHolder.getLocale()));
+        }
+        listPoke.sort(Comparator.comparingInt(PokemonType::getId));
+        return listPoke;
     }
 
-    @Override
-    public PokemonType getPokemonName (String name){ return pokemonTypeRepository.findPokemonTypeByName(name);}
+
+   @Override
+    public PokemonType getPokemonName (String name){
+        return pokemonTypeRepository.findPokemonTypeByName(name);}
+
+    public void setPokemonTypeRepository(PokemonTypeRepository pokemonTypeRepository) {
+        this.pokemonTypeRepository = pokemonTypeRepository;
+    }
+
+    public PokemonTypeRepository getPokemonTypeRepository() {
+        return pokemonTypeRepository;
+    }
+
+    public TranslationRepository getTranslationRepository() {
+        return translationRepository;
+    }
+
+    @Autowired
+    public void setTranslationRepository(TranslationRepository translationRepository) {
+        this.translationRepository = translationRepository;
+    }
 }
